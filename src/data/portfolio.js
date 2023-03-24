1ef06b3b8c880d17ba461bf1f6ff4f076f666a6b
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-24 21:09:32 +0530

    trying to fix

---
+++ src/data/portfolio.js
@@ -0,0 +1,38 @@
+export default [
+   {
+      title: 'Hangman Game',
+      imgUrl: '/assets/hangman-game.jfif',
+      stack: ['HTML', 'CSS', 'JavaScript','React'],
+      link: 'https://hangman-game-six-pi.vercel.app/',
+   },
+   //{
+     // title: 'Developer Portfolio',
+      //imgUrl: '/assets/dev-portfolio.jpg',
+      //stack: ['React', 'Tailwind', 'Vite'],
+      //link: 'https://.com',
+   //},
+   {
+      title: 'Movie App',
+      imgUrl: '/assets/movie-app.png',
+      stack: ['React', 'Tailwind', 'Node.js'],
+      link: 'https://github.com/ArslanYM/MovieAppReact',
+   },
+   {
+      title: 'Front-End Cheatsheet ',
+      imgUrl: '/assets/front-end.jpg',
+      stack: ['React', 'Tailwind', 'Node.js'],
+      link: 'https://github.com/ArslanYM/FrontendCheatsheet',
+   },
+   {
+      title: 'Javascript Learning Roadmap ',
+      imgUrl: '/assets/js.png',
+      stack: ['React', 'javascript', 'ES6'],
+      link: 'https://github.com/ArslanYM/JS--ReactJS',
+   },
+   {
+      title: 'Data-Structures and Algorithms ',
+      imgUrl: '/assets/dsa.webp',
+      stack: ['React', 'javascript', 'ES6'],
+      link: 'https://github.com/ArslanYM/JS--ReactJS',
+   },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,31 @@
+export default [
+   {
+      year: 'Today',
+      title: ' Open Source Contributor',
+      duration: ' Currently Contributing ',
+      details: 
+         ' I am a contributor to the open source project, React Play . I have contributed to the project by fixing bugs, adding new features, and improving the documentation. ',
+   },
+   {
+		year: 'Today',
+		title: 'Open Source Member',
+		duration: 'Currently Contributing',
+		details:
+			"I am a member of the open source project, if me . I have contributed to the project by fixing bugs, adding new features, and improving the documentation.",
+	},
+	{
+		year: '2023',
+		title: 'Engineer Intern',
+		duration: '1 Month',
+		details:
+			" I learnt about technologies like React , Flutter , Frameworks. I also learnt about the basics of : Flutter · HTML5 · HTML Scripting · Cascading Style Sheets (CSS) · JavaScript · HTML ",
+	},
+	{
+		year: '2022',
+		title: 'Student Intern at Robolabs India',
+		duration: '1 Month',
+		details:
+			" I learnt about technologies like React , Flutter , TailwindCSS . I also learnt about the basics of : Flutter · HTML5 · HTML Scripting · Cascading Style Sheets (CSS) · JavaScript · HTML ",
+	},
+	
+]
